c8651ff709827d9704e478063fb6b332ca759ed2
Prachi <prachijulka31@gmail.com>
2018-02-24 22:25:36 +0530

    Q4

--- GradleAssignment/src/main/java/Demo.java
+++ GradleAssignment/src/main/java/Demo.java
@@ -1,14 +1,16 @@
 //import org.apache.commons.lang3.StringUtils;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Demo {
     public static void main(String[] args) {
-    //    Demo1.method();
-       Demo1.display();
-/*        String str="Hello";
-        System.out.println(str);*/
+
+      // Demo1.display();
+        String str="Hello";
+        System.out.println(StringUtils.endsWith(str,"o"));
 
     }
 }
